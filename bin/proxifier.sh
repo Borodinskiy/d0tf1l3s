a9b17847b8ac73735bd0aa866edbf3c890ce15a8
Borodinskiy <ivanforest856@gmail.com>
2025-07-14 22:50:51 +0300

    Proxifier replacement algoritm change to awk :/

--- bin/proxifier.sh
+++ bin/proxifier.sh
@@ -3,16 +3,36 @@
 SCRIPT_HOME="$(dirname "$0")"
 [ "$HOME_WORKSPACE" ] && SCRIPT_HOME="$HOME_WORKSPACE/config/sing-box"
 
-CONFIG="/tmp/singbox-$USER.conf"
+CONFIG="/tmp/singbox-$USER.json"
 
 cd "$SCRIPT_HOME" || exit 1
 
 install -m 700 "config.json" "$CONFIG"
 
-sed \
-	-e '/\"<PROXY_PROCESSES>\"/{' -e 'r processes.json' -e 'd; }' \
-	-e '/\"<PROXY_DOMAINS>\"/{' -e 'r domains.json' -e 'd; }' \
-	-e '/\"<PROXY_OUTBOUND>\"/{' -e 'r proxy.json' -e 'd; }' \
-	"$SCRIPT_HOME/config.json" > "$CONFIG"
+# what is that
+awk '
+	BEGIN {
+		while ((getline line < "processes.json") > 0) {
+				processes_content = (processes_content ? processes_content "\n" : "") line
+		}
+		close("processes.json")
+		
+		while ((getline line < "domains.json") > 0) {
+				domains_content = (domains_content ? domains_content "\n" : "") line
+		}
+		close("domains.json")
+		
+		while ((getline line < "proxy.json") > 0) {
+				proxy_content = (proxy_content ? proxy_content "\n" : "") line
+		}
+		close("proxy.json")
+	}
+	{
+		gsub(/"<PROXY_PROCESSES>"/, processes_content)
+		gsub(/"<PROXY_DOMAINS>"/, domains_content)
+		gsub(/"<PROXY_OUTBOUND>"/, proxy_content)
+		print
+	}
+' "config.json" > "$CONFIG"
 
 sudo sing-box run --config "$CONFIG"

4d1a01b42b72b404a535feecdd1c548bdd2c9506
Borodinskiy <ivanforest856@gmail.com>
2025-07-22 09:56:00 +0300

    Proxifier: definitive edition

--- bin/proxifier.sh
+++ bin/proxifier.sh
@@ -19,22 +19,23 @@ handle_specific_settings() {
 	mv "$CONFIG.tmp" "$CONFIG"
 }
 
-SCRIPT_HOME="$(dirname "$0")"
-[ "$HOME_WORKSPACE" ] && SCRIPT_HOME="$HOME_WORKSPACE/config/sing-box"
+if [ -z "$HOME_WORKSPACE" ]; then
+	SCRIPT_HOME="$(dirname "$0")"
+else
+	SCRIPT_HOME="$HOME_WORKSPACE/config/sing-box"
+fi
 
-if { [ "$1" == "android" ] || [ "$1" == "windows" ]; } && [ "$CONFIG" == "" ]; then
+if [ -z "$CONFIG" ] && { [ "$1" == "android" ] || [ "$1" == "windows" ]; }; then
 	CONFIG="${HOME_MYFILES:-$HOME}/sing-box.json"
 else
 	CONFIG="${CONFIG:-/tmp/sing-box-$USER.json}"
 fi
 
 cd "$SCRIPT_HOME" || exit 1
-
-install -m 700 "/dev/null" "$CONFIG"
+install -m 700 "/dev/null" "$CONFIG" || exit 1
 
 awk '
 	function readfile(file, content, line) {
-		content = ""
 		while ((getline line < file) > 0) {
 			content = (content ? content "\n" : "") line
 		}
@@ -42,26 +43,18 @@ awk '
 		return content
 	}
 	BEGIN {
-		processes_proxy  = readfile("processes.json")
-		android_packages = readfile("android-packages.json")
-		domains_proxy    = readfile("domains-proxy.json")
-		domains_direct   = readfile("domains-direct.json")
-		outbound_proxy   = readfile("proxy.json")
+		outbound_proxy = readfile("proxy.json")
 	}
 	{
-		gsub(/"<PROCESSES_PROXY>"/, processes_proxy)
-		gsub(/"<ANDROID_PACKAGES>"/, android_packages)
-		gsub(/"<DOMAINS_PROXY>"/, domains_proxy)
-		gsub(/"<DOMAINS_DIRECT>"/, domains_direct)
 		gsub(/"<OUTBOUND_PROXY>"/, outbound_proxy)
 		print
 	}
-' "config.json" > "$CONFIG"
+' "config.jsonc" > "$CONFIG"
 
 if [ "$1" == "android" ] || [ "$1" == "windows" ]; then
 	handle_specific_settings "$1"
 	echo "Saved $1 configuration to $CONFIG"
 else
-	[ "$1" != "" ] && echo "Unknown system \"$1\""
+	[ ! -z "$1" ] && echo "Unknown system \"$1\""
 	sudo sing-box run --config "$CONFIG"
 fi

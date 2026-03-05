7442bb31e666667220fa93f15b7f86863dd707ab
Borodinskiy <ivanforest856@gmail.com>
2026-03-05 10:41:40 +0300

    Proxifier update

--- bin/proxifier.sh
+++ bin/proxifier.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ROUTING_TYPE="${ROUTING_TYPE:-remote}"
+
 if [ -z "$HOME_WORKSPACE" ]; then
 	CONFIG_DIR="$(dirname "$0")"
 else
@@ -11,9 +13,31 @@ ETC_DIR="/etc/sing-box"
 cd "$CONFIG_DIR" || exit 1
 echo "Copying config to $ETC_DIR. . ."
 
+sudo rm -f "$ETC_DIR"/*
 sudo mkdir -p "$ETC_DIR"
 
-sudo cp "config.json" "outbound-remote.json" "tun-linux.json" "$ETC_DIR"
+case "$ROUTING_TYPE" in
+	"amnezia")
+		ETC_CONFIGS=(
+			"config.json"
+			"outbound-direct.json"
+			"outbound-awg.json"
+			"proxy-server.json"
+			"tun-linux.json"
+		)
+		;;
+	"remote")
+		ETC_CONFIGS=(
+			"config.json"
+			"outbound-direct.json"
+			"outbound-remote.json"
+			"proxy-server.json"
+			"tun-linux.json"
+		)
+		;;
+esac
+
+sudo cp "${ETC_CONFIGS[@]}" "$ETC_DIR"
 
 if [ -f "$(which sing-box)" ]; then
 	echo "Found sing-box, checking configuration. . ."
